86ad61a9be70b7e0d38dcbdb1cc6a25e1def35c0
haniffsyahmi <148729374+haniffsyahmi@users.noreply.github.com>
2025-01-19 14:46:41 +0800

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -4,7 +4,7 @@ import 'movie.dart';
 import 'movie_service.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
